9677a41f995476fbcc88d30218f472b4fd57dd1b
MehulRoy0330 <mrcodes06@gmail.com>
2021-08-23 18:35:54 +0530

    Add background, ground and walls

--- sketch.js
+++ sketch.js
@@ -3,14 +3,27 @@ const Engine = Matter.Engine;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
+var engine, world;
 
+var ground, wall1, wall2, wall3;
 
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+    createCanvas(windowWidth, windowHeight);
+    engine = Engine.create();
+    world = engine.world;
+    ground = new Ground(width/2+150, height-50, width-300, 100);
+
+    wall1 = new Ground(75, height/2-120, 150, height-120);
+    wall2 = new Ground(width/2, 20, width, 40);
+    wall3 = new Ground(width-20, height/2, 40, height);
 }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+    background("#fff200");
+    Engine.update(engine);
+
+    ground.display();
+    wall1.display();
+    wall2.display();
+    wall3.display();
 }
